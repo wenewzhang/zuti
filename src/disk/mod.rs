9feb6387d29ee874cc6c8e8dd48cc9f7d5067fe7
Wenew Zhang <wenewboy@gmail.com>
2026-04-11 11:25:47 +0800

    free -> disk

--- src/disk/mod.rs
+++ src/disk/mod.rs
@@ -1,10 +1,10 @@
 /// 磁盘信息获取模块
 ///
 /// 提供获取系统磁盘信息和 ZFS 磁盘信息的功能
-pub mod free;
+pub mod disk;
 pub mod lsblk;
 pub mod zfs_utils;
 
-pub use free::get_free_disks;
+pub use disk::get_free_disks;
 pub use lsblk::{get_all_disks, DiskBasicInfo};
 pub use zfs_utils::{get_datasets, get_pool_devices, get_zfs_disks, DatasetDetail, PoolDevice};
